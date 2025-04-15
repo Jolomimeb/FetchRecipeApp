3b455d81570e8b885b22859877585d6bd9e3c1aa
Jolomimeb <mebaghanjejolomi@gmail.com>
2025-04-15 18:18:59 -0400

    Initial Commit

---
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppApp.swift
+//  FetchRecipeApp
+//
+//  Created by Jolomi Mebaghanje on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipeAppTests/FetchRecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeAppTests.swift
+//  FetchRecipeAppTests
+//
+//  Created by Jolomi Mebaghanje on 4/15/25.
+//
+
+import Testing
+@testable import FetchRecipeApp
+
+struct FetchRecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

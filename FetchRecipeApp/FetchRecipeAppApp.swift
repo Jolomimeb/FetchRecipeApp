0ea919328cfba712298b85d0832e83fa2843e38d
Jolomimeb <mebaghanjejolomi@gmail.com>
2025-04-16 13:07:12 -0400

    Update FetchRecipeAppApp.swift

--- FetchRecipeApp/FetchRecipeAppApp.swift
+++ FetchRecipeApp/FetchRecipeAppApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct FetchRecipeAppApp: App {
     var body: some Scene {
         WindowGroup {
+            // call the content view
             ContentView()
         }
     }
